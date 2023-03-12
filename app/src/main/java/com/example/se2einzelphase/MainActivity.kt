f4700a760c7deafba8797d1aad5946256a512202
deorhan <deorhan@edu.aau.at>
2023-03-12 15:00:55 +0100

    server response implementiert

--- app/src/main/java/com/example/se2einzelphase/MainActivity.kt
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.kt
@@ -3,10 +3,26 @@ package com.example.se2einzelphase
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.renderscript.ScriptGroup.Input
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.w3c.dom.Text
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.io.ObjectOutputStream
+import java.io.OutputStream
+import java.net.Inet4Address
+import java.net.Socket
+import java.util.Scanner
 import java.util.concurrent.ThreadLocalRandom
 import kotlin.random.Random
 
@@ -21,6 +37,8 @@ class MainActivity : AppCompatActivity() {
         var textErweiterung = findViewById<TextView>(R.id.textViewErweiterung)
         val textMatrikelnummer = findViewById<EditText>(R.id.editMatrikelnummer)
         val textGGT = findViewById<TextView>(R.id.textViewGGT)
+        var textServerAntwort = findViewById<TextView>(R.id.textViewServerAntwort)
+
         findViewById<Button>(R.id.buttonSend).setOnClickListener{
             if(textMatrikelnummer.text.toString() != "12119264"){
                 textErweiterung.text = "Falsche Matrikelnummer!"
@@ -29,7 +47,30 @@ class MainActivity : AppCompatActivity() {
                 val ggtIndex = findGGT(textMatrikelnummer.text.toString(), textGGT)
                 textErweiterung.text = ggtIndex
                 textErweiterung.setTextColor(Color.BLACK)
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    val antwort = client(address, port, textMatrikelnummer.text.toString())
+                    if (antwort != null) {
+                        textServerAntwort.text = antwort
+                    } else {
+                        textServerAntwort.text = "Error: Check Internet connection!"
+                    }
+                }
             }
+        }
+    }
+
+    private suspend fun client(address: String, port: Int, matrikelnummer: String): String? = withContext(IO) {
+        try {
+            val clientSocket = Socket(address, port)
+            val outputStream = ObjectOutputStream(clientSocket.getOutputStream())
+            val inputStream = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+
+            outputStream.writeUTF(matrikelnummer)
+            outputStream.flush()
+            return@withContext inputStream.buffered().readLine();
+        } catch (error: Exception) {
+            return@withContext null
         }
     }
 
